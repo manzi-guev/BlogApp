c83c44074a700a0f9be738cd6b11e84f8d297e47
Manzi Guevara <manziguevara@gmail.com>
2019-10-17 12:49:07 +0200

    Hosting app on gh-pages

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ mongoose.connect('mongodb://localhost/restful_blog_app', {
   useNewUrlParser: true,
   useUnifiedTopology: true
 });
+app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
 app.use(bodyParser.urlencoded({ extended: true }));
